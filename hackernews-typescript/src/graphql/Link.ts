4e052e69d413ba24ca6f2ab0612f44333acc8309
IbraheemLanre <s2001108@edu.bc.fi>
2023-01-16 12:48:00 +0200

    added votes logic, modified auth and update gitignore

--- hackernews-typescript/src/graphql/Link.ts
+++ hackernews-typescript/src/graphql/Link.ts
@@ -8,12 +8,20 @@ export const Link = objectType({
         t.nonNull.int("id")
         t.nonNull.string("description")
         t.nonNull.string("url")
+        t.nonNull.dateTime("createdAt")
         t.field("postedBy", {
             type: "User",
             resolve(parent, args, context) {
                 return context.prisma.link.findUnique({where: {id: parent.id}}).postedBy()
             }
-        })
+        }),
+            t.nonNull.list.nonNull.field("voters", {
+                type: "User",
+                resolve(parent, args, context) {
+                    return context.prisma.link.findUnique({where:{id:parent.id}}).voters()
+                }
+            })
+
     }
 })
 
@@ -40,13 +48,17 @@ export const LinkMutation = extendType({
             },
 
             resolve(parent, args, context) {
+                const { description, url } = args
+                const { userId } = context
+                if (!userId) {
+                    throw new Error("Cannot post without logging in.")
+                }
+
                 const newLink = context.prisma.link.create({
                     data: {
-                        description: args.description,
-                        url: args.url
-                    }
+                        description, url,
+                    postedBy: {connect: {id: userId}}}
                 })
-
                 return newLink
             }
         })
--- hackernews-typescript/src/graphql/User.ts
+++ hackernews-typescript/src/graphql/User.ts
@@ -12,6 +12,12 @@ export const User = objectType({
             resolve(parent, args, context) {
                 return context.prisma.user.findUnique({where: {id: parent.id}}).links()
             }
-        })
+        }),
+            t.nonNull.list.nonNull.field("votes", {
+                type: "Link",
+                resolve(parent, args, context) {
+                     context.prisma.user.findUnique({where: {id: parent.id}}).votes()
+                }
+            })
     },
 })
